b062954a285234aafa11f6526f57e6fc707f6404
hsolrac <ch.henriquevinha@gmail.com>
2024-04-18 18:00:32 -0300

    Simulation runtime

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,26 @@ impl Runtime {
 
 
 fn main() {
+    let mut runtime = Runtime::new();
+    let account_1 = "account_1".to_string();
+    let account_2 = "account_2".to_string();
+    let account_3 = "account_3".to_string();
+
+    runtime.balances.set_balance(account_1.clone(), 100);
+
+    // start block
+    runtime.system.increment_block_number(); 
+
+    //transactions
+    runtime.system.increment_nonce(account_1.clone());
+    let _result = runtime.balances
+        .transfer(account_1.clone(), account_2.clone(), 30)
+        .map_err(|e| println!("transfer failed: {:?}", e));
+
+    runtime.system.increment_nonce(account_2.clone());
+    let _result = runtime.balances
+        .transfer(account_1.clone(), account_3.clone(), 20)
+        .map_err(|e| println!("transfer failed: {:?}", e));
+
     println!("Done!");
 }
